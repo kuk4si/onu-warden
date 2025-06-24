c8cd5917e20088722be20decc14c354fd29cfeba
Andrey Medvedev <andrey.college2020@gmail.com>
2025-06-24 12:59:35 +0200

    remove print strings

--- onu_warden/manage.py
+++ onu_warden/manage.py
@@ -7,7 +7,6 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onu_warden.settings')
-    print(str(os.getenv('DJANGO_SECRET_KEY')))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
